bc367e891fc78c217b76c9178ef7dd5f02b98d84
Matt Jadud <matt@jadud.com>
2020-03-17 22:56:24 -0400

    Flipped to having objects check their constraints.
    This is nicer than the type-based checking in an interp loop. As long as
    I have classes and objects, I might as well use them.

--- constraints.py
+++ constraints.py
@@ -10,54 +10,31 @@ import structures as S
 
 @attr.s
 class Constraint (object):
-  pass
+  def check(self, course_set, desired_course):
+    pass
 
 @attr.s
 class NoCourseLevelConstraint (Constraint):
   course_level = attr.ib(type=int)
+  def check(constraint, course_set, desired_course):
+    return (desired_course.level == constraint.course_level)
 
 @attr.s
 class HasCourseLevelConstraint (Constraint):
   course_level   = attr.ib(type=int)
   required_level = attr.ib(type=int)
-
-@attr.s
-class SequenceConstraint (Constraint):
-  sequence = attr.ib(type=typing.List[S.Course])
-
-@attr.s
-class OneOfConstraint (Constraint):
-  course_set = attr.ib(type=typing.List[S.Course])
-
-@attr.s
-class AnyOfConstraint (Constraint):
-  course_set = attr.ib(type=typing.List[S.Course])
-
-@attr.s
-class LimitOfConstraint (Constraint):
-  count = attr.ib(type=int)
-  course_set = attr.ib(type=typing.List[S.Course])
-
-# Returns a boolean
-# Another way to do this would have been to allow
-# each object to have a .check(cs, desired) method.
-# Perhaps... hm. Perhaps that would have been better?
-# Dunno. TMTWWTDI. This will work better, I think, if I 
-# want to parse constraints in from a textual format.
-def interp(constraint, course_set, desired_course):
-  # NoCourseLevelConstraint
-  if isinstance(constraint, NoCourseLevelConstraint):
-    return (desired_course.level == constraint.course_level)
-  # HasCourseLevelConstraint
-  elif isinstance(constraint, HasCourseLevelConstraint):
+  def check(constraint, course_set, desired_course):
     meets_constraint = False
     if desired_course.level == constraint.course_level:
       for c in course_set:
         if c.level == constraint.required_level:
           meets_constraint = True
     return meets_constraint
-  # SequenceConstraint
-  elif isinstance(constraint, SequenceConstraint):
+
+@attr.s
+class SequenceConstraint (Constraint):
+  sequence = attr.ib(type=typing.List[S.Course])
+  def check(constraint, course_set, desired_course):
     all_in = True
     for constc in constraint.sequence:
       constr_in_set = False
@@ -66,39 +43,45 @@ def interp(constraint, course_set, desired_course):
           constr_in_set = True
       all_in = all_in and constr_in_set
     return all_in
-  elif isinstance(constraint, OneOfConstraint):
-    # If an empty set of courses is used as the constraint,
-    # then the student has one of them by definition.
-    if not constraint.course_set:
-      return True
-    else:
-      member = 0
-      for c in course_set:
-        for cc in constraint.course_set:
-          if c.get_id() ==  cc.get_id():
-            member = member + 1
-      return member == 1
-  elif isinstance(constraint, AnyOfConstraint):
-    if not constraint.course_set:
-      return True
-    else:
-      member = 0
-      for c in course_set:
-        for cc in constraint.course_set:
-          if c.get_id() ==  cc.get_id():
-            member = member + 1
-      return member > 0
-  elif isinstance(constraint, LimitOfConstraint):
-    if not constraint.course_set:
-      return True
-    else:
-      member = 0
-      for c in course_set:
-        for cc in constraint.course_set:
-          if c.get_id() ==  cc.get_id():
-            member = member + 1
-      return member >= constraint.count
-  else:
-    # If I find no reason to limit enrollment, then
-    # it's OK based on the constraints.
+
+def member_counter(constraint, course_set):
+  if not constraint.course_set:
     return True
+  else:
+    member = 0
+    for c in course_set:
+      for cc in constraint.course_set:
+        if c.get_id() ==  cc.get_id():
+          member = member + 1
+    return member
+    
+@attr.s
+class OneOfConstraint (Constraint):
+  course_set = attr.ib(type=typing.List[S.Course])
+  def check(constraint, course_set, desired_course):
+    return member_counter(constraint, course_set) == 1
+
+@attr.s
+class AnyOfConstraint (Constraint):
+  course_set = attr.ib(type=typing.List[S.Course])
+  def check(constraint, course_set, desired_course):
+    return member_counter(constraint, course_set) > 0
+
+@attr.s
+class LimitOfConstraint (Constraint):
+  count = attr.ib(type=int)
+  course_set = attr.ib(type=typing.List[S.Course])
+  def check(constraint, course_set, desired_course):
+    return member_counter(constraint, course_set) >= constraint.count
+
+# Returns a boolean
+# Another way to do this would have been to allow
+# each object to have a .check(cs, desired) method.
+# Perhaps... hm. Perhaps that would have been better?
+# Dunno. TMTWWTDI. This will work better, I think, if I 
+# want to parse constraints in from a textual format.
+#
+# UPDATE: I changed it so that the objects do their own checking.
+# I think this will be better for the overall app architecture.
+def interp(constraint, course_set, desired_course):
+  return constraint.check(course_set, desired_course)
